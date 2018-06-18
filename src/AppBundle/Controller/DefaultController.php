23d670c561df663d49ffb80549130df7214cebdb
Naash <hasan.taskin.pro@gmail.com>
2018-06-18 17:35:59 +0200

    select all equipe from database + need requete ajax pour recup joueurs

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
@@ -40,12 +42,50 @@ class DefaultController extends Controller
      * @Route("/coach", name="coachMenu")
      */
 
-     public function testAction(Request $request)
+     public function coachAction(Request $request)
      {
          // replace this example code with whatever you need
          return $this->render('coach/menu.html.twig', [
              'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
          ]);
+     }
+
+    /**
+     * @Route("/avantMatch", name="AvantMatch")
+     */
+
+     public function avantMatchAction(Request $request)
+     {
+
+           $em = $this->getDoctrine()->getManager();
+
+           $equipes = $em->getRepository('AppBundle:Equipes')->findAll();
+
+           return $this->render('clavier/AvantMatch.html.twig', array(
+               'equipes' => $equipes,
+           ));
+
+     }
+    /**
+     * @Route("/joueurByEquipe/{x}", name="joueurByEquipe")
+     */
+
+     public function joueurByEquipeAction(Request $request, $x)
+     {
+
+           $em = $this->getDoctrine()->getManager();
+           $id = $x;
+           $Joueur = $em->getRepository('AppBundle:Joueurs')->findBy(["equipeId" => $id]);
+        
+
+           function __ToString($Joueur){
+             return $this->Joueur;
+           };
+          return new Response($Joueur);
+        /*   return $this->render('equipes/index.html.twig', array(
+               'equipes' => $equipes, 'equipesId' => $equipeId
+           )); */
+
      }
 
     /**
